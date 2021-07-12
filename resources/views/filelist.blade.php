ef7148f04e7c23154ecfd809d4ea2a70361f585c
jim <jim@blueberry.nl>
2021-07-13 01:15:18 +0200

    filelist.blade.php fix icons

--- resources/views/filelist.blade.php
+++ resources/views/filelist.blade.php
@@ -6,9 +6,9 @@
 <body>
 <h1>Index of /{{$dir}}</h1>
 <h2>Move along, there's nothing to see here...</h2> <table>
-    <tr><th valign="top"><img src="/icons/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th><th><a href="?C=D;O=A">Description</a></th></tr>
+    <tr><th valign="top"><img src="/images/icons/blank.gif" alt="[ICO]"></th><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th><th><a href="?C=D;O=A">Description</a></th></tr>
     <tr><th colspan="5"><hr></th></tr>
-    <tr><td valign="top"><img src="/icons/back.gif" alt="[PARENTDIR]"></td><td><a href="/{{$dir}}/../">Parent Directory</a></td><td>&nbsp;</td><td align="right"> - </td><td>&nbsp;</td></tr>
+    <tr><td valign="top"><img src="/images/icons/back.gif" alt="[PARENTDIR]"></td><td><a href="/{{$dir}}/../">Parent Directory</a></td><td>&nbsp;</td><td align="right"> - </td><td>&nbsp;</td></tr>
 
     @foreach ($files as $file)
         <tr>
