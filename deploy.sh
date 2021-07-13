3dd8b78dd4cbd412a7cef3b95d1415dad0a61602
jim <jim@blueberry.nl>
2021-07-13 02:45:18 +0200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,12 @@ set -e
 # remove dev packages
 composer install --classmap-authoritative --no-dev
 
+# enable laravel routes caching
+./artisan route:cache
+
 # deploy
 serverless deploy
 
-
+# switch back to develop mode
+./artisan optimize:clear
+composer install
