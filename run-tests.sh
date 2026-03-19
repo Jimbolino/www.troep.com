88ecbeb28b71ed5bd400824270998f3b5ecf65c8
jim <jim@blueberry.nl>
2026-03-19 23:24:14 +0100

    run-test add echo

--- run-tests.sh
+++ run-tests.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 set -e
 
+echo '[php -l]'
 find . -type f -name '*.php' ! -path './vendor/*' -exec php -l -n {} \; | (! grep -v "No syntax errors detected" )
 
+echo '[phan]'
 PHAN_DISABLE_XDEBUG_WARN=1 ./vendor/bin/phan --no-progress-bar
 
+echo '[phpunit]'
 XDEBUG_MODE=coverage ./vendor/bin/phpunit --coverage-text --colors=never
 
+echo '[php-cs-fixer]'
 ./vendor/bin/php-cs-fixer fix --dry-run
 
 echo 'all tests passed!'
